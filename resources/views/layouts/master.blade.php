846c4b93b11e4f405d17c9d6168791b19966ccf0
Doug Bradley <dpb070@g.harvard.edu>
2016-11-25 15:07:02 -0500

    save point for working tests.index route (after app abort scare)

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Doug Bradley -->
+    <!-- CSCIE-15  -->
+    <!-- P4 -->
+    <meta charset="UTF-8">
+    <title>CSCIE-15 Project 4</title>
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.override.css" rel="stylesheet">
+    <link href="css/p4.css" rel="stylesheet">
+</head>
+<body>
+    @yield('navbarSection')
+    @yield('pageContentSection')
+<div class="navbar navbar-fixed-bottom">
+    <footer>
+        Doug Bradley
+        CSCIE-15
+        Project 4
+    </footer>
+</div>
+
+</body>
+</html>
--- resources/views/tests/index.blade.php
+++ resources/views/tests/index.blade.php
@@ -1,60 +1,63 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>CSCIE-15 Project 4</title>
-  <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/bootstrap.override.css" rel="stylesheet">
-  <link href="css/p4.css" rel="stylesheet">
-</head>
-
-<body>
-  <nav class="navbar navbar-default">
+@extends('layouts.master')
+@section('navbarSection')
+<nav class="navbar navbar-default">
     <div class="container-fluid">
-      <div class="navbar-header navbar-brand">
-        HealthTrack
-      </div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
+        <div class="navbar-header navbar-brand">
+            HealthTrack
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Tests</a></li>
+            <li><a href="#">Results</a></li>
+        </ul>
     </div>
-  </nav>
+</nav>
+@stop
+@section('pageContentSection')
 
-Hello
+<div class="control_section">
+    <button type="button" class="btn btn-success btn-sm">
+        <span class="fa fa-plus-circle"></span> New
+    </button>
+</div>
+
 <table class="table-bordered table-striped">
     <tbody>
+        <thead>
+            <tr>
+                <td> </td>
+                <td>Test</td>
+                <td>Units</td>
+                <td>Validation Low Limit</td>
+                <td>Validation High Limit</td>
+                <td>Default Low Warning</td>
+                <td>Default High Warning</td>
+                <td>Comments</td>
+                <td> </td>
+            </tr>
+        </thead>
+
         @foreach($tests as $test)
         <tr>
+            <td>
+                <button type="button" class="btn btn-info btn-xs">
+                    <span class="fa fa-pencil-square-o"></span> Edit
+                </button>
+            </td>
             <td>{{ $test->name }}</td>
             <td>{{ $test->units }}</td>
+            <td>{{ $test->validation_low_limit }}</td>
+            <td>{{ $test->validation_high_limit }}</td>
+            <td>{{ $test->default_low_warning }}</td>
+            <td>{{ $test->default_high_warning }}</td>
+            <td>{{ $test->comments }}</td>
+            <td>
+                <button type="button" class="btn btn-danger btn-xs">
+                    <span class="fa fa-minus-circle"></span> Delete
+                </button>
+            </td>
         </tr>
         @endforeach
     </tbody>
 </table>
-
-
-  <!-- @if (isset($firstName))
-  <table>
-    <thead>
-      <tr>
-        <td>First Name</td>
-        <td>Last Name</td>
-      </tr>
-    </thead>
-    <tbody>
-      @for ($i = 0; $i < $userCount; $i++)
-      <tr>
-        <td>{{ $firstName[$i] }}</td>
-        <td>{{ $lastName[$i] }}</td>
-      </tr>
-      @endfor
-    </tbody>
-  </table>
-  @endif -->
-
-</body>
+@stop
