ef77474db51cfe7e98944d92024b619682bb7707
Doug Bradley <dpb070@g.harvard.edu>
2016-11-28 23:20:40 -0500

    break time - sticky footer addition

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,19 +9,18 @@
     <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/bootstrap.override.css" rel="stylesheet">
-    <link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">    
+    <link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">
     <link href="/css/p4.css" rel="stylesheet">
 </head>
 <body>
     @yield('navbarSection')
     @yield('pageContentSection')
-<div class="navbar navbar-fixed-bottom">
+
     <footer>
         Doug Bradley
         CSCIE-15
         Project 4
     </footer>
-</div>
 
 </body>
 </html>
