75d74536c505de7fe00eb8b267341d927e6a0534
Doug Bradley <dpb070@g.harvard.edu>
2016-12-09 11:53:06 -0500

    menus working w/ admin and user roles

--- resources/views/includes/rhNavbar.blade.php
+++ resources/views/includes/rhNavbar.blade.php
@@ -1,4 +1,7 @@
 <ul class="nav navbar-nav navbar-right">
     <li><a href="/users/{{ $appUser->id}}/edit">{{ $appUser->name or ' '}}</a></li>
+    @if ( $appUser->role == 'ADMIN' )
+        <li> <a href="/users">Users</a></li>
+    @endif
     <li><a href="/logout">Logout</a></li>
 </ul>
--- resources/views/includes/testsNavbar.blade.php
+++ resources/views/includes/testsNavbar.blade.php
@@ -5,7 +5,6 @@
             <li><a href="/results">Results</a></li>
             <li class="active"><a href="/tests">Tests</a></li>
             <li><a href="/warninglimits">Warning Limits</a></li>
-            <li><a href="/users">Users</a></li>
         </ul>
         @include('includes.rhNavbar')
     </div>
