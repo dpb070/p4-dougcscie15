a4d35d7c39ac9595252f8a0050994f99556fb247
Doug Bradley <dpb070@g.harvard.edu>
2016-12-12 21:15:30 -0500

    corrected panel names

--- resources/views/users/delete.blade.php
+++ resources/views/users/delete.blade.php
@@ -9,7 +9,7 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading panel-title">Results</div>
+        <div class="panel-heading panel-title">Users</div>
         <div class="panel-body">
 
           <div class="panel-heading">
