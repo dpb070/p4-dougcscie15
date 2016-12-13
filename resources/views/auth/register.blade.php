a0a69d16b93960bc2d960c3507adb45922b344e1
Doug Bradley <dpb070@g.harvard.edu>
2016-12-12 21:23:42 -0500

    correct registration page menu includes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.master')
+@include('includes.loginNavbar')
+@section('pageContentSection')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
